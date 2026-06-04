3417024939f38643c100b3a8e1897c7fe5806653
focusbp <info@soshiki-kaikaku.com>
2026-06-04 15:15:14 +0900

    Use app tmp for BCP export

--- fbp/app/bcp_export/bcp_export.php
+++ fbp/app/bcp_export/bcp_export.php
@@ -48,7 +48,7 @@ class bcp_export {
 	}
 
 	private function resolve_temp_dir(): string {
-		return rtrim($this->project_root, "/") . "/classes/log/tmp/bcp_export";
+		return rtrim($this->project_root, "/") . "/tmp/bcp_export";
 	}
 
 	private function assert_project_root(): void {
@@ -204,6 +204,7 @@ class bcp_export {
 				".git/",
 				"templates_c/",
 				"classes/log/tmp/",
+				"tmp/bcp_export/",
 				"temporary export zip",
 			],
 		];
